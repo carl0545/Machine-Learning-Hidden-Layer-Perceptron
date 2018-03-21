669b63eab7574db92ac01622ff5f7034723bc050
carl0545 <carl0545@vandals.uidaho.edu>
2018-03-21 13:51:49 -0700

    Setting basic values for inputs, hidden nodes, tr_rows, tr_cols.

--- nn.cpp
+++ nn.cpp
@@ -10,6 +10,40 @@
 
 using namespace std;
 
+void setBasics(int*, int*, int*, int*); //Function reads in total inputs, rows and column for the matrix from stdin
+
+
 int main(){
 
+  //////
+  initRand();
+
+  //////////INPUT OF TRAINING AND TESTING MATRIX///////
+  int train_inputs, train_rows, train_cols, test_rows, test_cols;
+  int target_rows, target_cols;
+  int hidden_nodes;
+  Matrix train, test, target;
+
+  ///Training Matrix
+  setBasics(&train_inputs, &train_rows, &train_cols, &hidden_nodes);
+
+  cout << "Hidden nodes: " << hidden_nodes << endl;
+
+
+}
+
+/*
+*Function reads in total inputs, rows and column for the matrix from stdin
+*/
+void setBasics(int *input, int *rows, int *cols, int *hidden){
+
+  if(input != NULL){ //input not recieved for the testing matrix
+    cin >> *input;
+  }
+  if(hidden != NULL){
+    cin >> *hidden;
+  }
+
+  cin >> *rows;
+  cin >> *cols;
 }

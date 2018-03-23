bfb7c4246980c0a6c5e62a791270f67998a200ef
carl0545 <carl0545@vandals.uidaho.edu>
2018-03-22 19:56:04 -0700

    Input of matrices added, and target matrix added

--- nn.cpp
+++ nn.cpp
@@ -11,7 +11,8 @@
 using namespace std;
 
 void setBasics(int*, int*, int*, int*); //Function reads in total inputs, rows and column for the matrix from stdin
-
+void setValuesf(Matrix*, bool); //Function sets the elements of the matrix from stdin
+void setTargetValues(Matrix*, Matrix); //Function grabs the target values from the training matrix and assigns them to the target matrix
 
 int main(){
 
@@ -26,9 +27,26 @@ int main(){
 
   ///Training Matrix
   setBasics(&train_inputs, &train_rows, &train_cols, &hidden_nodes);
+  train_cols++; //bias
+  train = new Matrix(train_rows, train_cols, "train");
+  setValuesf(&train, true);
 
-  cout << "Hidden nodes: " << hidden_nodes << endl;
 
+  ///Testing Matrix
+  setBasics(NULL, &test_rows, &test_cols, NULL);
+  test_cols++; //bias
+  test = new Matrix(test_rows, test_cols, "test");
+  setValuesf(&test, true);
+
+  ///Target Matrix
+  target_cols = train_cols - test_cols;
+  target_rows = train_rows;
+
+  target = new Matrix(target_rows, target_cols, "target");
+
+  setTargetValues(&target, train);
+
+  target.print();
 
 }
 
@@ -46,4 +64,56 @@ void setBasics(int *input, int *rows, int *cols, int *hidden){
 
   cin >> *rows;
   cin >> *cols;
+}
+
+
+/*
+*Function sets the elements of the matrix from stdin
+*/
+void setValuesf(Matrix *m, bool bias){
+
+  if(bias){
+    for(int r = 0; r < m->numRows(); r++){
+      for(int c = 0; c < m->numCols(); c++){
+        if(c == 0){
+          m->set(r,c,-1.0);
+          continue;
+        }
+        double element;
+        cin >> element;
+        m->set(r,c, element);
+      }
+    }
+
+  }
+
+  else{
+
+    for(int r = 0; r < m->numRows(); r++){
+      for(int c = 0; c < m->numCols(); c++){
+        double element;
+        cin >> element;
+        m->set(r,c, element);
+      }
+    }
+  }
+
+
+}
+
+/*
+*Function grabs the target values from the training matrix and assigns them to the target matrix
+*/
+void setTargetValues(Matrix *target, Matrix training){
+  int starting_col;
+
+  starting_col = training.numCols() - target->numCols();
+
+  for(int r = 0; r < target->numRows(); r++){
+    for(int c = starting_col; c - starting_col < target->numCols(); c++){
+      target->set(r,c - starting_col, training.get(r,c));
+    }
+  }
+
+
 }
